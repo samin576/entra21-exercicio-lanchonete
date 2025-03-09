e263fc2ab27f3afc139f8c844c3c7659f2949737
Samin Ghoddosi <saminghoddosi44@gmail.com>
2025-03-09 16:44:59 -0300

    NET 7 -> 8

--- AppLanchonete/Pedido.cs
+++ AppLanchonete/Pedido.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppLanchonete
+namespace sd
 {
-    class Pedido
+    public class Pedido
     {
+        public int NumeroPedido;
+        public List<Produto> listaProduto = new List<Produto>();
+        public double PrecoTotal()
+        {
+            double soma = 0;
+            foreach (var produto in listaProduto)
+            {
+                soma += produto.PrecoProduto;
+            }
+            return soma;
+        }
+        public void Lista()
+        {
+            Console.WriteLine($"Pedido #{NumeroPedido}");
+            Console.WriteLine("Itens:");
+            foreach (var produto in listaProduto)
+            {
+                Console.WriteLine(produto.NomeProduto);
+            }
+            Console.WriteLine($"\nTotal é de {PrecoTotal():F2} reais\n");
+        }
+
     }
 }
--- AppLanchonete/Program.cs
+++ AppLanchonete/Program.cs
@@ -1,5 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Oi");
-Console.WriteLine("Tchau");
-Console.WriteLine("Tchau");
+﻿using sd;
+using System;
+class Program
+{
+    static void Main(string[] Args)
+    {
+
+        Produto hamburguer = new Produto();
+        hamburguer.NomeProduto = "hambúrguer";
+        hamburguer.PrecoProduto = 8.99;
+
+        Produto peixe = new Produto();
+        peixe.NomeProduto = "peixe";
+        peixe.PrecoProduto = 7;
+
+        Produto agua = new Produto();
+        agua.NomeProduto = "água";
+        agua.PrecoProduto = 5;
+
+        Produto coca = new Produto();
+        coca.NomeProduto = "coca-cola";
+        coca.PrecoProduto = 8;
+
+        Pedido pedido1 = new Pedido();
+        pedido1.NumeroPedido = 1;
+        pedido1.listaProduto.Add(hamburguer);
+        pedido1.listaProduto.Add(coca);
+        pedido1.listaProduto.Add(peixe);
+        pedido1.listaProduto.Add(agua);
+        pedido1.Lista();
+
+        Pedido pedido2 = new Pedido();
+        pedido2.NumeroPedido = 2;
+        pedido2.listaProduto.Add(agua);
+        pedido2.Lista();
+
+
+    }
+}
