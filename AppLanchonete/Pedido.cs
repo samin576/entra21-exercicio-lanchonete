d1b7f3a6f26d67eb8d21d62a9c1c2d9d801104d6
Samin Ghoddosi <saminghoddosi44@gmail.com>
2025-03-11 16:23:27 -0300

    Mudando

--- AppLanchonete/Pedido.cs
+++ AppLanchonete/Pedido.cs
@@ -11,26 +11,93 @@ namespace AppLanchonete
     public class Pedido
     {
         public int NumeroPedido;
-        public List<Produto> listaProduto = new List<Produto>();
+        List<Produto> produtos = new List<Produto>();
         public double PrecoTotal()
         {
             double soma = 0;
-            foreach (var produto in listaProduto)
+            foreach (var produto in produtos)
             {
                 soma += produto.PrecoProduto;
             }
             return soma;
         }
+        public void Itens()
+        {
+
+            Console.WriteLine("As opções de produto são:");
+            Console.WriteLine("1 - hambúrguer");
+            Console.WriteLine("2 - peixe");
+            Console.WriteLine("3 - água");
+            Console.WriteLine("4 - coca-cola");
+            Console.WriteLine("Seriam quantos produtos?");
+            int numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+            {
+                Console.WriteLine("Digite o número do produto:");
+                int produto = int.Parse(Console.ReadLine());
+                produtos.Add(produto);
+            }
+        }
+        public void Produtos(List<int> n1)
+        {
+            Produto _1 = new Produto();
+            Produto _2 = new Produto();
+            Produto _3 = new Produto();
+            Produto _4 = new Produto();
+            int fim =1;
+            do
+            {
+                for (int i = 0; i < n1.Count; i++)
+                {
+
+                    switch (n1[i])
+                    {
+
+                        case 1:
+                            _1.NomeProduto = "hambúrguer";
+                            _1.PrecoProduto = 8.99;
+                            break;
+                        case 2:
+                            _2.NomeProduto = "peixe";
+                            _2.PrecoProduto = 7;
+                            break;
+                        case 3:
+                            _3.NomeProduto = "água";
+                            _3.PrecoProduto = 5;
+                            break;
+                        case 4:
+                            _4.NomeProduto = "coca-cola";
+                            _4.PrecoProduto = 8;
+                            break;
+                        default:
+                            Console.WriteLine("Você não digitiou nenhum produto cadastrado!");
+                            fim = 1;
+                            break;
+
+                    }
+                }
+            } while (fim!=1);
+
+
+
+
+
+
+        }
+
+
         public void Lista()
         {
             Console.WriteLine($"Pedido #{NumeroPedido}");
             Console.WriteLine("Itens:");
-            foreach (var produto in listaProduto)
+            foreach (var produto in produtos)
             {
                 Console.WriteLine(produto.NomeProduto);
             }
             Console.WriteLine($"\nTotal é de {PrecoTotal():F2} reais\n");
         }
 
+
     }
+
 }
--- AppLanchonete/Program.cs
+++ AppLanchonete/Program.cs
@@ -5,34 +5,16 @@ class Program
     static void Main(string[] Args)
     {
 
-        Produto hamburguer = new Produto();
-        hamburguer.NomeProduto = "hambúrguer";
-        hamburguer.PrecoProduto = 8.99;
-
-        Produto peixe = new Produto();
-        peixe.NomeProduto = "peixe";
-        peixe.PrecoProduto = 7;
-
-        Produto agua = new Produto();
-        agua.NomeProduto = "água";
-        agua.PrecoProduto = 5;
-
-        Produto coca = new Produto();
-        coca.NomeProduto = "coca-cola";
-        coca.PrecoProduto = 8;
 
         Pedido pedido1 = new Pedido();
         pedido1.NumeroPedido = 1;
-        pedido1.listaProduto.Add(hamburguer);
-        pedido1.listaProduto.Add(coca);
-        pedido1.listaProduto.Add(peixe);
-        pedido1.listaProduto.Add(agua);
+        pedido1.Itens();
         pedido1.Lista();
 
-        Pedido pedido2 = new Pedido();
-        pedido2.NumeroPedido = 2;
-        pedido2.listaProduto.Add(agua);
-        pedido2.Lista();
+        //Pedido pedido2 = new Pedido();
+        //pedido2.NumeroPedido = 2;
+        //pedido2.listaProduto.Add(agua);
+        //pedido2.Lista();
 
 
     }
