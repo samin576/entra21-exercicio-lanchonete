016edae8fbfb10823ea068f81d3a15ab6c3a8742
Samin Ghoddosi <saminghoddosi44@gmail.com>
2025-03-12 17:49:07 -0300

    Colocando menu

--- AppLanchonete/Program.cs
+++ AppLanchonete/Program.cs
@@ -6,35 +6,86 @@ class Program
 {
     static void Main(string[] Args)
     {
-
-
-        string decisao;
-        int n1 = 1;
+        operacoes operacoes = new operacoes();
+        int operacao;
         do
         {
-           
-            for (int i = 0; i <n1 ; i++)
+            Menu();
+            operacao = int.Parse(Console.ReadLine());
+            Console.Clear();
+            Escolha(operacao, operacoes);
+            if (operacao >= 0 && operacao <= 4)
             {
-                Pedido pedido1 = new Pedido();
-                pedido1.NumeroPedido = n1;
-                pedido1.Itens();
-                pedido1.Lista();
+                Console.Clear();
             }
+
+
+        } while (operacao != 0);
+        Console.WriteLine("Tudo bem! Adeus!");
+       
+
+
+        //string decisao;
+        //int n1 = 1;
+        //do
+        //{
+
+        //    for (int i = 0; i < n1; i++)
+        //    {
+        //        Pedido pedido1 = new Pedido();
+        //        pedido1.NumeroPedido = n1;
+        //        pedido1.Itens();
+        //        pedido1.Lista();
+        //    }
+
+        //    Console.WriteLine("\nSe quiser sair, digite q. Senão, digite qualquer outra coisa");
+        //    decisao = Console.ReadLine().ToLower();
+        //    if (decisao != "q")
+        //    {
+        //        n1++;
+        //    }
+
+
+
+
+
+
+        //} while (decisao != "q");
+        //Console.WriteLine("Tudo bem. Tchau!");
+    }
+    static void Menu()
+    {
+        Console.WriteLine("1 - Cadastrar novo produto");
+        Console.WriteLine("2 - Listar produto");
+        Console.WriteLine("3 - Fazer pedido");
+        Console.WriteLine("4 - Lista");
+        Console.WriteLine("0 - Sair");
+
+    }
+    static void Escolha(int n1, operacoes operacoes)
+    {
+       
+
+        switch (n1)
+        {
             
-            Console.WriteLine("\nSe quiser sair, digite q. Senão, digite qualquer outra coisa");
-            decisao = Console.ReadLine().ToLower();
-            if (decisao != "q")
-            {
-                n1++;
-            }
-           
-           
+            case 1:
+                operacoes.CadastrarNovoProduto();
+                break;
+            case 2:
+                operacoes.ListarProdutos();
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 0:
+                break;
+            default:
+                Console.WriteLine("Você não digitou nenhuma opção válida!");
+                break;
+            
 
-
-        } while (decisao != "q");
-        Console.WriteLine("Tudo bem. Tchau!");
-
-      
-
+        }
     }
 }
---
+++ AppLanchonete/operacoes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLanchonete
+{
+    class operacoes
+    {
+       public  List<Produto> produtos = new List<Produto>();
+        public void CadastrarNovoProduto()
+        {
+            
+            Produto novosProdutos = new Produto();
+            Console.WriteLine("Digite o nome do produto:");
+            novosProdutos.NomeProduto = Console.ReadLine();
+            Console.WriteLine("Digite o preço:");
+            novosProdutos.PrecoProduto = double.Parse(Console.ReadLine());
+            produtos.Add(novosProdutos);
+            Console.WriteLine("Produto cadastrado!");
+        }
+        public void ListarProdutos()
+        {
+            if (produtos.Count == 0)
+            {
+                Console.WriteLine("Nenhum produto cadastrado!");
+            }
+            else
+            {
+                for (int i = 0; i < produtos.Count; i++)
+                {
+                    Console.WriteLine($"{i}- {produtos[i].NomeProduto} = {produtos[i].PrecoProduto}");
+
+                }
+            }
+                //foreach(var i in produtos)// ver o que é var
+            
+        }
+    }
+}
